bc8ef7432eedb8a95f8cb7e631a88c787f989b2e
Oleg Khristenk <olegkhristenko@gmail.com>
2024-02-25 19:38:43 +0200

    Finished Auth page

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,4 +1,5 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
+import { VisibilityOff, Visibility } from '@mui/icons-material';
 
 interface IProps {
     id: string;
@@ -10,9 +11,24 @@ interface IProps {
 }
 
 const Input: FC<IProps> = ({ id, type, label, title, value, onChange }) => {
+    const [showPassword, setShowPassword] = useState(false);
+
+    const getTypes = (type: string) => {
+        if (type === 'password') {
+            return showPassword ? 'text' : 'password';
+        }
+
+        return type;
+    };
+
     return (
         <div className="input" title={title}>
-            <input id={id} type={type} value={value} required onChange={onChange} />
+            <input id={id} type={getTypes(type)} value={value} required onChange={onChange} />
+            {type === 'password' && (
+                <button type="button" onClick={() => setShowPassword(prevState => !prevState)}>
+                    {showPassword ? <VisibilityOff sx={{ color: '#1f1f23' }} /> : <Visibility sx={{ color: '#1f1f23' }} />}
+                </button>
+            )}
             <label htmlFor={id}>{label}</label>
             <div className="background"></div>
         </div>
--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,5 +1,5 @@
-import React, { FC, MouseEvent, useState } from 'react';
-import { VisibilityOff, Visibility } from '@mui/icons-material';
+import React, { FC, useState } from 'react';
+import { Login, Favorite } from '@mui/icons-material';
 import Button from '../components/Button';
 import { useAppDispatch } from '../store/hook';
 import { registration, login } from '../store/my-user/thunks';
@@ -12,26 +12,19 @@ const Auth: FC = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [repeatPassword, setRepeatPassword] = useState('');
-    const [showPassword, setShowPassword] = useState(false);
-    const [showRepeatPassword, setShowRepeatPassword] = useState(false);
-
-    const handleClickShowPassword = () => setShowPassword((show) => !show);
-
-    const handleMouseDownPassword = (event: MouseEvent<HTMLButtonElement>) => {
-        event.preventDefault();
-    };
 
     const dispatch = useAppDispatch();
 
     return (
-        <div className="page">
+        <div className="page auth-page">
             <Card
                 withLights
+                classes="rolled-up"
                 title={
                     <h1 className="title">
-                        <i className="fa-solid fa-right-to-bracket"></i>
+                        <Login className="login" sx={{ color: '#45f3ff' }} />
                         Привіт! {isRegistration ? 'Давай знайомитись. Моє ім\'я Олег.' : 'Нагадай хто ти?'}
-                        <i className="fa-solid fa-heart"></i>
+                        <Favorite className="favorite" sx={{ color: '#ff2770' }} />
                     </h1>
                 }
             >
@@ -55,7 +48,7 @@ const Auth: FC = () => {
                 />
                 <Input
                     id="password"
-                    type={showPassword ? 'text' : 'password'}
+                    type="password"
                     label="Пароль*"
                     title="Це поле потрібне щоб ніхто інший не зміг скористуватись твоїми данними."
                     value={password}
@@ -63,8 +56,8 @@ const Auth: FC = () => {
                 />
                 {isRegistration && (
                     <Input
-                        id="name"
-                        type={showRepeatPassword ? 'text' : 'password'}
+                        id="repeat-password"
+                        type="password"
                         label="Повтори пароль*"
                         title="Це поле потрібне щоб ти не помилився при введені паролю."
                         value={repeatPassword}
@@ -82,93 +75,6 @@ const Auth: FC = () => {
                     <Button onClick={() => dispatch(login({ email, password }))}>Увійти</Button>
                 )}
             </Card>
-
-            {/*<Wrap>*/}
-
-                {/*<FormControl*/}
-                {/*    sx={{ width: '100%' }}*/}
-                {/*    variant="outlined"*/}
-                {/*    size="small"*/}
-                {/*    title="Це поле потрібне щоб я міг ідентифікувати тебе."*/}
-                {/*>*/}
-                {/*    <InputLabel htmlFor="email">Email</InputLabel>*/}
-                {/*    <OutlinedInput*/}
-                {/*        id="email"*/}
-                {/*        type="text"*/}
-                {/*        label="Email"*/}
-                {/*        value={email}*/}
-                {/*        onChange={(e) => setEmail(e.target.value)}*/}
-                {/*    />*/}
-                {/*</FormControl>*/}
-
-                {/*<FormControl*/}
-                {/*    sx={{ width: '100%' }}*/}
-                {/*    variant="outlined"*/}
-                {/*    size="small"*/}
-                {/*    title="Це поле потрібне щоб ніхто інший не зміг скористуватись твоїми данними."*/}
-                {/*>*/}
-                {/*    <InputLabel htmlFor="password">Пароль</InputLabel>*/}
-                {/*    <OutlinedInput*/}
-                {/*        id="password"*/}
-                {/*        type={showPassword ? 'text' : 'password'}*/}
-                {/*        autoComplete="current-password"*/}
-                {/*        label="Пароль"*/}
-                {/*        value={password}*/}
-                {/*        onChange={(e) => setPassword(e.target.value)}*/}
-                {/*        endAdornment={*/}
-                {/*            <InputAdornment position="end">*/}
-                {/*                <IconButton*/}
-                {/*                    aria-label="toggle password visibility"*/}
-                {/*                    onClick={handleClickShowPassword}*/}
-                {/*                    onMouseDown={handleMouseDownPassword}*/}
-                {/*                    edge="end"*/}
-                {/*                >*/}
-                {/*                    {showPassword ? <VisibilityOff /> : <Visibility />}*/}
-                {/*                </IconButton>*/}
-                {/*            </InputAdornment>*/}
-                {/*        }*/}
-                {/*    />*/}
-                {/*</FormControl>*/}
-
-                {/*{isRegistration && (*/}
-                {/*    <FormControl*/}
-                {/*        sx={{ width: '100%' }}*/}
-                {/*        variant="outlined"*/}
-                {/*        size="small"*/}
-                {/*        title="Це поле потрібне щоб ти не помилився при введені паролю."*/}
-                {/*    >*/}
-                {/*        <InputLabel htmlFor="repeatPassword">Повторіть пароль</InputLabel>*/}
-                {/*        <OutlinedInput*/}
-                {/*            id="repeatPassword"*/}
-                {/*            type={showRepeatPassword ? 'text' : 'password'}*/}
-                {/*            autoComplete="current-password"*/}
-                {/*            label="Повторіть пароль"*/}
-                {/*            endAdornment={*/}
-                {/*                <InputAdornment position="end">*/}
-                {/*                    <IconButton*/}
-                {/*                        aria-label="toggle password visibility"*/}
-                {/*                        onClick={() => setShowRepeatPassword((show) => !show)}*/}
-                {/*                        onMouseDown={handleMouseDownPassword}*/}
-                {/*                        edge="end"*/}
-                {/*                    >*/}
-                {/*                        {showRepeatPassword ? <VisibilityOff /> : <Visibility />}*/}
-                {/*                    </IconButton>*/}
-                {/*                </InputAdornment>*/}
-                {/*            }*/}
-                {/*        />*/}
-                {/*    </FormControl>*/}
-                {/*)}*/}
-
-                {/*<ToggleRegistration type="button" onClick={() => setIsRegistration((state) => !state)}>*/}
-                {/*    {isRegistration ? 'Ми вже знайомі.' : 'Ти мене ще не знаєш.'}*/}
-                {/*</ToggleRegistration>*/}
-
-                {/*{isRegistration ? (*/}
-                {/*    <Button onClick={() => dispatch(registration({ name, email, password }))}>Зареєструватися</Button>*/}
-                {/*) : (*/}
-                {/*    <Button onClick={() => dispatch(login({ email, password }))}>Увійти</Button>*/}
-                {/*)}*/}
-            {/*</Wrap>*/}
         </div>
     );
 };
