ff9efd5a6222d8ff6e1d770729797a00ec6c9ef9
Oleg Khristenk <olegkhristenko@gmail.com>
2024-04-01 21:01:22 +0300

    made first variant welcome page

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,86 +1,3 @@
-//import React, { FC } from 'react';
-//import Button from '@/components/Button';
-//import Logo from '@/components/Logo';
-//
-//const Welcome: FC = () => {
-//    return (
-//        <div className="welcome-page">
-//            <div className="welcome-page-header">
-//                <Logo />
-//
-//                <div className="actions">
-//                    <Button to="/auth" variant="text">Увійти</Button>
-//                    <Button to="/auth?register">Зареєструватись</Button>
-//                </div>
-//            </div>
-//
-//            <section className="cover">
-//                <div className="info-content">
-//                    <p className="cover-text">
-//                        Вітаю! Моє ім’я Олег. Цей застосунок створено для тебе.
-//                    </p>
-//                    <p className="cover-text">
-//                        <span>У тебе є бажання?</span>
-//                        <span>Про що ти мрієш?</span>
-//                        <span>
-//                            Чи не було б чудово мати особистого помічника у збиранні та втіленні всіх своїх мрій?
-//                        </span>
-//                        <span>Дорога та потужна техніка?</span>
-//                        <span>Люксові автівки?</span>
-//                        <span>Подорожі в різні кутки планети?</span>
-//                    </p>
-//                </div>
-//
-//                <div className="bg-img cover">
-//                    <div className="linear-gradient top"></div>
-//                    <div className="linear-gradient right"></div>
-//                    <div className="linear-gradient bottom"></div>
-//                    <div className="linear-gradient left"></div>
-//
-//                    <div className="img"></div>
-//                </div>
-//            </section>
-//
-//            <section className="info">
-//                <p className="info-text">
-//                    Напевно, ні для кого не секрет, що головною рушійною силою виконання бажань є думки.
-//                    А задокументоване бажання виконується в кілька раз швидше. Цей застосунок допоможе тобі в цьому.
-//                    Він цілком присвячений твоїм бажанням. Створені бажання ти можеш показувати тільки своїм друзям,
-//                    всім користувачам або ж нікому. І найголовніше.
-//                    Ідучи до когось на свято більше не потрібно випитувати "Що тобі подарувати?".
-//                    Просто поділись посиланням та попроси додати кілька бажань.
-//                </p>
-//            </section>
-//
-//            <section className="end">
-//                <div className="bg-img end">
-//                    <div className="linear-gradient top"></div>
-//                    <div className="linear-gradient right"></div>
-//                    <div className="linear-gradient bottom"></div>
-//                    <div className="linear-gradient left"></div>
-//
-//                    <div className="img"></div>
-//                </div>
-//
-//                <div className="info-content">
-//                    <p className="info-text">
-//                        Напевно, ні для кого не секрет, що головною рушійною силою виконання бажань є думки.
-//                        А задокументоване бажання виконується в кілька раз швидше. Цей застосунок допоможе тобі в цьому.
-//                        Він цілком присвячений твоїм бажанням. Створені бажання ти можеш показувати тільки своїм друзям,
-//                        всім користувачам або ж нікому. І найголовніше.
-//                        Ідучи до когось на свято більше не потрібно випитувати "Що тобі подарувати?".
-//                        Просто поділись посиланням та попроси додати кілька бажань.
-//                    </p>
-//                </div>
-//            </section>
-//        </div>
-//    );
-//};
-//
-//export default Welcome;
-
-//******************************************
-
 import React, { FC } from 'react';
 import Button from '@/components/Button';
 import Logo from '@/components/Logo';
@@ -98,43 +15,21 @@ const Welcome: FC = () => {
             </div>
 
             <section className="section">
-                <div className="info-content">
-                    <p className="section-text">
-                        Вітаю! Моє ім’я Олег. Цей застосунок створено для тебе.
-                    </p>
-                    <p className="section-text">
-                        <span>У тебе є бажання? Про що ти мрієш?</span>
-                        <span>
-                            Чи не було б чудово мати особистого помічника у збиранні та втіленні всіх своїх мрій?
-                        </span>
-                    </p>
-                </div>
+                <p className="section-text">
+                    Вітаю!
+                    <br />
+                    Я твій особистий помічник у здійсненні бажань!
+                    <br />
+                    Про що ти мрієш?
+                </p>
 
-                <div className="bg-img">
-                    <div className="shadow bottom"></div>
-
-                    <div className="img"></div>
-                </div>
-            </section>
-
-            <section className="section">
-                <div className="bg-img">
-                    <div className="shadow top"></div>
-                    <div className="shadow bottom"></div>
-
-                    <div className="img second"></div>
-                </div>
-
-                <div className="info-content">
-                    <p className="section-text sm">
-                        Напевно, ні для кого не секрет, що головною рушійною силою виконання бажань є думки.
-                        А задокументоване бажання виконується в кілька раз швидше. Цей застосунок допоможе тобі в цьому.
-                        Він цілком присвячений твоїм бажанням. Створені бажання ти можеш показувати тільки своїм друзям,
-                        всім користувачам або ж нікому. І найголовніше.
-                        Ідучи до когось на свято більше не потрібно випитувати "Що тобі подарувати?".
-                        Просто поділись посиланням та попроси додати кілька бажань.
-                    </p>
-                </div>
+                <p className="section-text sm">
+                    Ти, мабуть, знаєш, що думки матеріальні. А записані бажання виконуються набагато швидше.
+                    Запиши сюди всі свої великі мрії і маленькі хотілочки.
+                    Ти можеш ділитися ними зі своїми друзями або залишити їх лише для себе)
+                    І найважливіше - ідучи до когось на свято не потрібно ламати голову над подарунками
+                    - ти тут зможеш знайти список того чого хоче твій друг)
+                </p>
             </section>
         </div>
     );
