52192e32ddbd213beb5ce71493029f17aa073f22
Oleg Khristenk <olegkhristenko@gmail.com>
2024-05-09 10:16:09 +0300

    returned encryption

--- src/utils/encryption-data.ts
+++ src/utils/encryption-data.ts
@@ -1,28 +1,29 @@
 import CryptoJS from 'crypto-js';
 import { IWish } from '@/models/IWish';
 
-// export const encryptedData = (data: string, secret: string): string => CryptoJS.AES.encrypt(data, secret).toString();
-export const encryptedData = (data: string, secret: string): string => data;
-export const decryptedData = (data: string, secret: string): string => data;
+export const encryptedData = (data: string, secret: string): string => CryptoJS.AES.encrypt(data, secret).toString();
 
-// export const decryptedData = (data: string, secret: string): string => {
-//     try {
-//         const decrypted = CryptoJS.AES.decrypt(data, secret).toString(CryptoJS.enc.Utf8);
-//         if (!decrypted) {
-//             throw new Error('Не вдалося розшифрувати');
-//         }
-//         return decrypted;
-//     } catch (error) {
-//         return data;
-//     }
-// };
+export const decryptedData = (data: string, secret: string): string => {
+    try {
+        const decrypted = CryptoJS.AES.decrypt(data, secret).toString(CryptoJS.enc.Utf8);
+        if (!decrypted) {
+            throw new Error('Не вдалося розшифрувати');
+        }
+        return decrypted;
+    } catch (error) {
+        return data;
+    }
+};
+
+//export const encryptedData = (data: string, secret: string): string => data;
+//export const decryptedData = (data: string, secret: string): string => data;
 
 export const unencryptedData = (data: string, show: IWish['show']): string => {
-    if (!data || !process.env.REACT_APP_CRYPTO_JS_SECRET) {
+    if (!data) {
         return '';
     }
 
-    if (show === 'all') {
+    if (show === 'all' || !process.env.REACT_APP_CRYPTO_JS_SECRET) {
         return data;
     }
 
