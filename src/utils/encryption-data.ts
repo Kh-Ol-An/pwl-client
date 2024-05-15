86209f4db1f9c00668eb114fd2f606474a651553
Oleg Khristenk <olegkhristenko@gmail.com>
2024-05-15 23:09:36 +0300

    check encryption error

--- src/utils/encryption-data.ts
+++ src/utils/encryption-data.ts
@@ -4,7 +4,15 @@ import { IWish } from '@/models/IWish';
 
 export const encryptedData = (data: string, secret: string): string => CryptoJS.AES.encrypt(data, secret).toString();
 
-export const decryptedData = (data: string, secret: string): string => {
+export const decryptedData = (data: string, secret: string, wish?: any): string => {
+    if (wish.id === '6620f3aaa436b3d1205f0e6e') {
+        console.log('---------------------------------');
+        console.log('decryptedData');
+        console.log('wish: ', wish);
+        console.log('data: ', data);
+        console.log('secret: ', secret);
+        console.log('---------------------------------');
+    }
     try {
         const decrypted = CryptoJS.AES.decrypt(data, secret).toString(CryptoJS.enc.Utf8);
         if (!decrypted) {
@@ -16,7 +24,16 @@ export const decryptedData = (data: string, secret: string): string => {
     }
 };
 
-export const unencryptedData = (data: string, show: IWish['show']): string => {
+export const unencryptedData = (data: string, show: IWish['show'], wish?: any): string => {
+    if (wish.id === '6620f3aaa436b3d1205f0e6e') {
+        console.log('---------------------------------');
+        console.log('unencryptedData');
+        console.log('wish: ', wish);
+        console.log('data: ', data);
+        console.log('show: ', show);
+        console.log('env: ', !process.env.REACT_APP_CRYPTO_JS_SECRET);
+        console.log('---------------------------------');
+    }
     if (!data) {
         return '';
     }
